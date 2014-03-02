f4d8821d6c77721a0d706f596449a23a5eb79021
Matt Morse <mmorse@comcast.net>
2014-03-02 10:28:15 -0700

    styling and tween anima

---
+++ public/Javascripts/main.js
@@ -0,0 +1,47 @@
+$(function(){
+
+
+$(document).ready(function(){
+	var logo = document.getElementById("bar-slide");
+    TweenLite.to(logo,2, {left:"200px"});
+	});
+
+
+// $(document).ready(function() {
+//     var logo = document.getElementById("bar-slide");
+//     TweenLite.to(logo, 2, {right:"200px", ease:SlowMo.ease});
+// });
+
+$(document).ready(function(){
+	var logo = document.getElementById("logo-slide");
+    TweenLite.to(logo,3, {left:"100px"});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
