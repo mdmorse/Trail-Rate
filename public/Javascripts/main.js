e3c2d3caea25467e1042451c3656ed9b51739013
Matt Morse <mmorse@comcast.net>
2014-03-05 23:00:39 -0700

    added icons

--- public/Javascripts/main.js
+++ public/Javascripts/main.js
@@ -40,8 +40,8 @@ $(function(){
 
 	function initialize() {
         var mapOptions = {
-          center: new google.maps.LatLng(39.646941, -105.196378),
-          zoom: 10
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
