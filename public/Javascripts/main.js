2a7896189a42072591c283da80a1dd4d839bb7be
mdmorse <mmorse@comcast.net>
2014-03-05 18:35:59 -0700

    commit

--- public/Javascripts/main.js
+++ public/Javascripts/main.js
@@ -1,6 +1,7 @@
 $(function(){
 
 
+	
 
 	$(window).load(function(){
 		var logo = document.getElementById("logo-slide");
@@ -16,14 +17,38 @@ $(function(){
 		$('.trail-name').delay('1000').slideDown('slow')
 	});
 
+	$(function(){
+   $("#map-slide-btn").click(function () {
+      $(this).text(function(i, text){
+          return text === "More Info" ? "Less Info" : "More Info";
+      })
+   });
+})
+
 	$('.trail-name').click(function(){
 		$('.description').slideToggle('fast');
+		// $('#map-slide').text('Show Map');
+	});
+	
+	$('#map-slide-btn').click(function() {
+		$('#map-container').slideToggle('slow');
+		// $('#map-slide').text('Hide Map');
 	});
 
-	// $(document).ready(function() {
-	//     var location = document.getElementById("location-slide");
-	//     TweenLite.to(location, 2, {right:"200px", ease:SlowMo.ease});
-	// });
+	
+
+
+	function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(39.646941, -105.196378),
+          zoom: 10
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+
+	
 
 
 
