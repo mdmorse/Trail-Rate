e28cd831dcde509aaf8623ca93d76da45ce82817
Matt Morse <mmorse@comcast.net>
2014-03-14 17:32:15 -0600

    cleane up main.js

--- public/Javascripts/main.js
+++ public/Javascripts/main.js
@@ -13,8 +13,19 @@ $(function(){
 			$(this).text(function(i, text){
 				return text === "More Info" ? "Less Info" : "More Info";
 			})
-			$(this).closest('.trail-item').find('.map-container').slideToggle();
+			$(this).closest('.trail-item').find('.map-container').slideToggle();				
 	});
+
+	
+	function initialize(){
+	        	var mapOptions = {
+	          	center: new google.maps.LatLng(-34.397, 150.644),
+	          	zoom: 8
+	        };
+	        var map = new google.maps.Map(document.getElementById("map-canvas"),
+	            mapOptions);
+	     	}
+	google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
@@ -42,69 +53,25 @@ $(function(){
 		    		$('.trail-info-wrapper').empty();
 
 		    		for(i = 0; i < data.length; i++){
-		    			var newItem = $("<div class = 'trail-item'>");
+		    			var newItem = $("<div class = 'trail-item'>"); 
 		    			newItem.append('<h3>' + data[i].name + '</h3>');
 		    			newItem.append('<p>' + data[i].description + '</p>');
 		    			newItem.append('<div class = "btn btn-success more-info-btn">More Info</div>');
-		    			newItem.append('<div class = "map-container"><div id = "map-canvas'+ i +'"></div></div>');
+		    			newItem.append('<div class = "map-container"><div id = "map-canvas"></div></div>');
 		    			newItem.appendTo('.trail-info-wrapper');
 		    		}
 		    		
-		    		$('.main-trail-container').first().delay('1000').slideDown('slow');
-		    		// $('.trail-info-wrapper').append('<div class = "btn btn-default map-slide-btn">More Info</div>' );
-
-
+		    		$('.main-trail-container').show();
 		    	}		
 		    })
-});		
-}
+		});		
+	}
 
 displayInfo();
 
 
 
-//**************************creates map***************************
 
-	$(document).on('click','.more-info-btn',function(){
-		$(this).next('.map-container').showToggle('slow',function(){
-			initialize();				
-		});
-	});
-
-	function initialize(){
-	        	var mapOptions = {
-	          	center: new google.maps.LatLng(-34.397, 150.644),
-	          	zoom: 8
-	        };
-
-	        var map = new google.maps.Map(document.getElementById("map-canvas"),
-	            mapOptions);
-	     	}
-	
-
-
-	google.maps.event.addDomListener(window, 'load', initialize);
-	
-	
-
-
-
-	
-
-
-
-
-
-
-//******************creates marker on map*****************
-
-  //     var marker=new google.maps.Marker({
-  // 		position:myCenter,
-  // 		});
-
-		// marker.setMap(map);
-
-	
 
 
 
